c78b9b188265233ef9ae4835a86f0dfbfecb16a0
IElizaI <lizakorneva1997@gmail.com>
2022-04-27 00:28:39 +0300

    4 files modified, 1 added

--- src/main.jsx
+++ src/main.jsx
@@ -11,9 +11,20 @@ let rootElement = document.getElementById('root');
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="expenses" element={<Expenses />} />
-      <Route path="invoices" element={<Invoices />} />
+      <Route path="/" element={<App />}>
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />}>
+          <Route path=":invoiceId" element={<Invoice />} />
+        </Route>
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: '1rem' }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
+      </Route>
     </Routes>
   </BrowserRouter>,
   rootElement
